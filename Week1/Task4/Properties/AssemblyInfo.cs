ef79494261457968bb61e7e73f84de56c372d3a5
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2019-01-31 19:28:01 +0600

    initial

--- Week1/Task4/Properties/AssemblyInfo.cs
+++ Week1/Task4/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Task1")]
+[assembly: AssemblyTitle("Task4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task1")]
+[assembly: AssemblyProduct("Task4")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("f8544eeb-7827-44e2-87f9-4b530593b03f")]
+[assembly: Guid("b5e949b9-6bd4-4866-acdb-5524de0e1b9f")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
