12e8f67b94db711b7391f8ea3726ff676dcacf69
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2019-01-31 20:50:31 +0600

    TS1V1

--- Week1/Task1/Program.cs
+++ Week1/Task1/Program.cs
@@ -10,6 +10,12 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
