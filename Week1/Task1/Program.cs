a232c61a8557c92c774c092713e1e008824508ec
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2019-02-01 11:19:53 +0600

    TS1 Mod

--- Week1/Task1/Program.cs
+++ Week1/Task1/Program.cs
@@ -8,13 +8,42 @@ namespace Task1
 {
     class Program
     {
+        public static bool primeN(int x)
+        {
+            if (x <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
+            int cnt = 0;
             int size = Convert.ToInt32(Console.ReadLine());
             int[] arr = new int[size];
+            string s = Console.ReadLine();
+            arr = s.Split(' ').Select(int.Parse).ToArray();
             for (int i = 0; i < arr.Length; i++)
             {
-                arr[i] = Convert.ToInt32(Console.ReadLine());
+                if (primeN(arr[i]) == true)
+                {
+                    cnt++;
+                }
+            }
+            Console.WriteLine(cnt);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (primeN(arr[i]) == true)
+                {
+                    Console.Write(arr[i] + " ");
+                }
             }
         }
     }
