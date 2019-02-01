6b66f96ecb218804c3e6cb2d4a3d06914565a8ae
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2019-02-01 11:35:29 +0600

    TS3 Mod

--- Week1/Task3/Program.cs
+++ Week1/Task3/Program.cs
@@ -10,6 +10,16 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            string x = Console.ReadLine();
+            int ss = int.Parse(x);
+            int[] array = new int[ss];
+            string str = Console.ReadLine();
+            array = str.Split(' ').Select(int.Parse).ToArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+                Console.Write(array[i] + " ");
+            }
         }
     }
 }
