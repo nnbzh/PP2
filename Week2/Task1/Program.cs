e922ca0ffa5b905996f499c2517c34e66e3b1dc8
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2019-02-01 13:33:49 +0600

    hello

--- Week2/Task1/Program.cs
+++ Week2/Task1/Program.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task4
+namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string x = Console.ReadLine();
-            int ss = int.Parse(x);
-            int[] array = new int[ss];
         }
     }
 }
