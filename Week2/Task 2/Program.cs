8369eca95ed9eb87986f492207da19a784512f0c
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2019-02-04 23:53:40 +0600

    uod

---
+++ Week2/Task 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static bool primeN(int x)
+        {
+            if (x <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {;
+            string s = System.IO.File.ReadAllText(@"C:\Users\Nurik\Desktop\pp2\Week2\test\TS2.txt");
+            Console.WriteLine(s);
+            int[] arr = new int[s.Length/2 + 1];
+            arr = s.Split(' ').Select(int.Parse).ToArray();
+            for (int i = 0; i < arr.Length;i++)
+            {
+                if (primeN(arr[i]) == true) {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+    }
+}
