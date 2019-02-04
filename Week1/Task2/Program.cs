d6158aefd5627e9102ce3ade95d3f4e361f962cb
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2019-02-04 22:57:32 +0600

    W2TS1

--- Week1/Task2/Program.cs
+++ Week1/Task2/Program.cs
@@ -6,14 +6,32 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    class student
+    class Student
     {
+        public string name;
+        public int id;
+        public int year;
 
+        public Student(string name, int id, int year)
+        {
+            this.name = name;
+            this.id = id;
+            this.year = year;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(id);
+            Console.WriteLine(name);
+            Console.WriteLine(year);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Student s1 = new Student("Berik", 1 , 1);
+            s1.PrintInfo();
         }
     }
 }
--- Week2/Task1/Program.cs
+++ Week2/Task1/Program.cs
@@ -8,8 +8,28 @@ namespace Task1
 {
     class Program
     {
+        private static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
         static void Main(string[] args)
         {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Nurik\Desktop\pp2\Week2\test\test.txt");
+            System.Console.WriteLine(text);
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Nurik\Desktop\pp2\Week2\test\test.txt");
+            string word = lines[0];
+            string reversed = Reverse(word);
+            if (word == reversed)
+            {
+                Console.WriteLine("Yes");
+            } else
+            {
+                Console.WriteLine("No");
+            }
+
         }
     }
 }
