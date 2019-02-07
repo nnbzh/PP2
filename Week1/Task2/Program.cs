8c79c46b91080196a35f66fee8191a1e9f2600bd
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2019-02-07 17:28:01 +0600

    upd

--- Week1/Task2/Program.cs
+++ Week1/Task2/Program.cs
@@ -12,26 +12,36 @@ namespace Task2
         public int id;
         public int year;
 
-        public Student(string name, int id, int year)
+        public Student(string name, int id, int year) //constructor
         {
             this.name = name;
             this.id = id;
             this.year = year;
         }
 
-        public void PrintInfo()
+        public void PrintInfo() //method to access ID, name and year
         {
-            Console.WriteLine(id);
-            Console.WriteLine(name);
-            Console.WriteLine(year);
+            Console.WriteLine("Student ID is: "+id);
+            Console.WriteLine("The name of the student is: " + name);
+            Console.WriteLine("He/She is " + year + " student.");
         }
+
+        
     }
     class Program
     {
+        public static Student incYear(Student student)
+        {
+            student.year = student.year + 1;
+            return student;
+        }
         static void Main(string[] args)
         {
             Student s1 = new Student("Berik", 1, 1);
             Student s2 = new Student("Nurik", 2, 1);
+            s1.PrintInfo();
+            incYear(s1);
+            s1.PrintInfo();
         }
     }
 }
